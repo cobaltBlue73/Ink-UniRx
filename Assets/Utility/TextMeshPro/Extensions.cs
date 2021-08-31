aba8ae446ab913be2c1f4787ae7e6c53a407caa3
CobaltBlue73 <md.zaid73@gmail.com>
2021-08-31 23:48:00 +0800

    Added TextMeshPro Extensions

---
+++ Assets/Utility/TextMeshPro/Extensions.cs
@@ -0,0 +1,36 @@
+using TMPro;
+
+namespace Utility.TextMeshPro
+{
+    public static class Extensions
+    {
+        public static bool IsLetterOrDigit(this TMP_CharacterInfo info) => char.IsLetterOrDigit(info.character);
+        
+        public static bool IsLetter(this TMP_CharacterInfo info) => char.IsLetter(info.character);
+        
+        public static bool IsDigit(this TMP_CharacterInfo info) => char.IsDigit(info.character);
+        
+        public static bool IsNumber(this TMP_CharacterInfo info) => char.IsNumber(info.character);
+        
+        public static bool IsControl(this TMP_CharacterInfo info) => char.IsControl(info.character);
+        
+        public static bool IsLower(this TMP_CharacterInfo info) => char.IsLower(info.character);
+        
+        public static bool IsPunctuation(this TMP_CharacterInfo info) => char.IsPunctuation(info.character);
+        
+        public static bool IsSeparator(this TMP_CharacterInfo info) => char.IsSeparator(info.character);
+        
+        public static bool IsSurrogate(this TMP_CharacterInfo info) => char.IsSurrogate(info.character);
+        
+        public static bool IsUpper(this TMP_CharacterInfo info) => char.IsUpper(info.character);
+        
+        public static bool IsHighSurrogate(this TMP_CharacterInfo info) => char.IsHighSurrogate(info.character);
+        
+        public static bool IsLowSurrogate(this TMP_CharacterInfo info) => char.IsLowSurrogate(info.character);
+        
+        public static bool IsSymbol(this TMP_CharacterInfo info) => char.IsSymbol(info.character);
+        
+        public static bool IsWhiteSpace(this TMP_CharacterInfo info) => char.IsWhiteSpace(info.character);
+        
+    }
+}
